cf90f718a77eba7f1db63ec598fbb17274d3c886
Han-jinwook <chiu3@naver.com>
2025-12-10 17:05:33 +0900

    Bump version to 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.bes2.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.4"
+        versionCode = 5
+        versionName = "1.5"
 
         testInstrumentationRunner = "com.bes2.app.HiltTestRunner"
         vectorDrawables {
